ac36ec43d13644dda985aa2b1f37d804cb06e285
ycm7089 <ycm7089@naver.com>
2022-06-09 02:19:11 +0900

    useless code delete , time comparison and fix the header opencv2/core/core.hpp

--- yolov5_pytorch_ros/src/cm_detect.cpp
+++ yolov5_pytorch_ros/src/cm_detect.cpp
@@ -15,9 +15,14 @@
 #include <detection_msgs/BoundingBox.h>
 #include <detection_msgs/BoundingBoxes.h>
 
+// below is fatal error: opencv2/core/core.hpp: No such file or directory
+// #include <opencv2/core/core.hpp>
+
+using namespace std;
+
 typedef struct
 {
-    char box_class;
+    string box_class;
     double box_probability;
     int box_xmin;
     int box_ymin;
@@ -31,8 +36,8 @@ typedef message_filters::sync_policies::ApproximateTime<detection_msgs::Bounding
 class cm_detect
 {
 public:
-    detection_msgs::BoundingBox BoundingBox_msg;
-    detection_msgs::BoundingBoxes BoundingBoxes_msg;
+    // detection_msgs::BoundingBox BoundingBox_msg;
+    // detection_msgs::BoundingBoxes BoundingBoxes_msg;
 
     BoundingBox_Info bounding_info;
     
@@ -63,11 +68,33 @@ void cm_detect::realcallback(const detection_msgs::BoundingBoxesConstPtr& bbox, 
 
     // Below code is a comparison bbox time and depth time because we want to know to use ApproximateTime
     // ROS_WARN_STREAM(bbox->header.stamp.sec);
-    // std::cerr << "BBox time : " << bbox -> header.stamp.sec << std::endl;
-    // std::cerr << "Depth Image time : " << depth -> header.stamp.sec << std::endl;
+    // std::cout << "BBox time : " << bbox -> header.stamp.sec << std::endl;
+    // std::cout << "Depth Image time : " << depth -> header.stamp.sec << std::endl;
+
+    // subscribe the bbox data
+    bounding_info.box_class = bbox -> bounding_boxes[0].Class;
+    bounding_info.box_probability = bbox -> bounding_boxes[0].probability;
+    bounding_info.box_xmin = bbox -> bounding_boxes[0].xmin;
+    bounding_info.box_ymin = bbox -> bounding_boxes[0].ymin;
+    bounding_info.box_xmax = bbox -> bounding_boxes[0].xmax;
+    bounding_info.box_ymax = bbox -> bounding_boxes[0].ymax;
+
+    // https://cafepurple.tistory.com/46?category=1044213 depth info
+    // std::cout << "Depth Info" << std::endl;
+    // std::cout << "Size : " << depth.size() << std::endl;
+    // std::cout << "Row, Col : " << depth.rows << depth.cols << std::endl;
+    // std::cout << "Channels : " << depth.channels() << std::endl;
+    // std::cout << "Element : " << depth.depth() << std::endl;
+    // std::cout << "ElementSize : " << depth.elemSize() << std::endl;
+    // std::cout << "Type : " << depth.type() << std::endl; // CV_8UC1 = 0
 
     // convert depth image in bbox to pointcloud
-
+    if(bounding_info.box_class == "person" & bounding_info.box_probability >= 0.7)
+    {
+        ROS_WARN_STREAM("Yes");
+        // cv::Mat convert_image;
+        // convert_image.converTo();
+    }
 }
 
 
