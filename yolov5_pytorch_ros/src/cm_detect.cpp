bce11cff70ab612b46071f9b3f9e5cea8d7792d5
ycm7089 <ycm7089>
2022-06-09 17:23:22 +0900

    z

--- yolov5_pytorch_ros/src/cm_detect.cpp
+++ yolov5_pytorch_ros/src/cm_detect.cpp
@@ -5,6 +5,7 @@
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CompressedImage.h>
+#include <sensor_msgs/image_encodings.h>
 
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
@@ -17,12 +18,15 @@
 
 // below is fatal error: opencv2/core/core.hpp: No such file or directory
 // #include <opencv2/core/core.h>
-
-using namespace std;
+#include <opencv2/highgui/highgui.hpp>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 
 typedef struct
 {
-    string box_class;
+    std::string box_class;
     double box_probability;
     int box_xmin;
     int box_ymin;
@@ -36,8 +40,6 @@ typedef message_filters::sync_policies::ApproximateTime<detection_msgs::Bounding
 class cm_detect
 {
 public:
-    // detection_msgs::BoundingBox BoundingBox_msg;
-    // detection_msgs::BoundingBoxes BoundingBoxes_msg;
 
     BoundingBox_Info bounding_info;
     
@@ -48,54 +50,87 @@ public:
     cm_detect(ros::NodeHandle *nh)
         : sync(MySyncPolicy(100), bbox_sub, depth_sub)
     {
-        std::cerr << "cm_detect()" << std::endl;
-
         bbox_sub.subscribe(*nh, "/yolov5/detections", 100);
         depth_sub.subscribe(*nh, "/camera/depth/image_rect_raw", 100);
 
         sync.registerCallback(boost::bind(&cm_detect::realcallback, this,_1, _2));
-        ROS_WARN_STREAM("z");
     }
 
 public:
-   
+   double f_x = 469.45703125;
+   double f_y = 468.9375;
+   double c_x = 353.50390625;
+   double c_y = 243.98046875;
+
+
     void realcallback(const detection_msgs::BoundingBoxesConstPtr& bbox, const sensor_msgs::ImageConstPtr& depth);
 };
 
 void cm_detect::realcallback(const detection_msgs::BoundingBoxesConstPtr& bbox, const sensor_msgs::ImageConstPtr& depth)
 {
-    ROS_WARN_STREAM("T.T");
-
-    // Below code is a comparison bbox time and depth time because we want to know to use ApproximateTime
-    ROS_WARN_STREAM(bbox -> header.stamp.sec);
-    ROS_WARN_STREAM(depth -> header.stamp.sec);
-    
-    std::cout << "BBox time : " << bbox -> header.stamp.sec << std::endl;
-    std::cout << "Depth Image time : " << depth -> header.stamp.sec << std::endl;
-
     // subscribe the bbox data
-    bounding_info.box_class = bbox -> bounding_boxes[0].Class;
-    bounding_info.box_probability = bbox -> bounding_boxes[0].probability;
-    bounding_info.box_xmin = bbox -> bounding_boxes[0].xmin;
-    bounding_info.box_ymin = bbox -> bounding_boxes[0].ymin;
-    bounding_info.box_xmax = bbox -> bounding_boxes[0].xmax;
-    bounding_info.box_ymax = bbox -> bounding_boxes[0].ymax;
+    ROS_WARN_STREAM("===========");
+    int size = bbox -> bounding_boxes.size();
+    if (size > 0)
+    {
+        bounding_info.box_class = bbox -> bounding_boxes[0].Class;
+        bounding_info.box_probability = bbox -> bounding_boxes[0].probability;
+        bounding_info.box_xmin = bbox -> bounding_boxes[0].xmin;
+        bounding_info.box_ymin = bbox -> bounding_boxes[0].ymin;
+        bounding_info.box_xmax = bbox -> bounding_boxes[0].xmax;
+        bounding_info.box_ymax = bbox -> bounding_boxes[0].ymax;
 
-    // https://cafepurple.tistory.com/46?category=1044213 depth info
-    // std::cout << "Depth Info" << std::endl;
-    // std::cout << "Size : " << depth.size() << std::endl;
-    // std::cout << "Row, Col : " << depth.rows << depth.cols << std::endl;
-    // std::cout << "Channels : " << depth.channels() << std::endl;
-    // std::cout << "Element : " << depth.depth() << std::endl;
-    // std::cout << "ElementSize : " << depth.elemSize() << std::endl;
-    // std::cout << "Type : " << depth.type() << std::endl; // CV_8UC1 = 0
+        // if (bounding_info.box_xmin < 0) bounding_info.box_xmin = 0;
+        // if (bounding_info.box_xmax > 1279) bounding_info.box_xmax = 1279;
+        // if (bounding_info.box_ymin < 0) bounding_info.box_ymin = 0;
+        // if (bounding_info.box_ymax > 719) bounding_info.box_ymax = 719;
 
+        ROS_INFO("x min : %d" ,bounding_info.box_xmin);
+        ROS_INFO("x max : %d", bounding_info.box_xmax);
+
+        ROS_INFO("y min : %d", bounding_info.box_ymin);
+        ROS_INFO("y max : %d", bounding_info.box_ymax);
+    
+        cv_bridge::CvImagePtr cv_ptr;
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(depth, sensor_msgs::image_encodings::TYPE_16UC1); 
+        }
+        catch(cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception : %s", e.what());
+            return;
+        }
+
+        cv::Mat& depth_mat = cv_ptr->image;
+        // std::cout << "zz : " << depth_mat.size() << std::endl;
+
+        double avr_v = (bounding_info.box_ymin + bounding_info.box_ymax)/2;
+        double avr_u = (bounding_info.box_xmin + bounding_info.box_xmax)/2;
+
+        for(int u = bounding_info.box_xmin; u < bounding_info.box_xmax; u++)
+        {
+            for(int v = bounding_info.box_ymin; v < bounding_info.box_ymax; v++)    
+            {
+                double z = depth_mat.at<unsigned short>(avr_v ,avr_u) * 0.001; // unit : [m]
+                // float z = depth_mat.at<unsigned short>(v ,u) * 0.001;
+                if (z!=0) 
+                {
+                    // std::cout << "depth_unsigned_short : " << z << "m"<< std::endl;
+
+                    double x = (u - c_x) * z / f_x;
+                    double y = (v - c_y) * z / f_y;
+
+                    std::cout << "x : " << x << " y : " << y << std::endl;
+                }
+            }
+        }
+    }
+    
     // convert depth image in bbox to pointcloud
     if(bounding_info.box_class == "person" & bounding_info.box_probability >= 0.5)
     {
-        ROS_WARN_STREAM("Yes");
-        // cv::Mat convert_image;
-        // convert_image.converTo();
+        // ROS_WARN_STREAM("Yes");
     }
 }
 
@@ -112,7 +147,6 @@ int main(int argc, char **argv)
     ros::Rate loop_rate(1); //Hz
     while(ros::ok())
     {
-        // data_sub.Tf_publisher();            
         ros::spinOnce();
 
         loop_rate.sleep();
